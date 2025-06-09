88710abd62cdcc012ba5ffde33552509d437e8d3
Mayank Sharma <1611mash@gmail.com>
2025-06-09 21:08:55 +0530

    Initial Commit

---
+++ my-fish-app/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+
+import Header from "@/components/Header";
+// import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "My Fish Market App",
+  description: "A complete marketplace to buy fish",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {/* <Navbar/> */}
+        <Header />
+
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
