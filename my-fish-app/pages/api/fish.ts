a3aab423cfce4bf6e0ef7fb6716162f86c38b42d
Mayank Sharma <1611mash@gmail.com>
2025-09-14 11:48:01 +0530

    Deleted extra fish.ts route file

--- my-fish-app/pages/api/fish.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import fishData from "../../mock-data/fish.json";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    return res.status(200).json(fishData);
-  }
-  return res.status(405).json({ message: "Method not allowed" });
-}
